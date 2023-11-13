a3f9873241d02956edeb1aeaa8b1ec6f38fc3117
mouhamadoufallbah <mouhamadoufall.bah@gmail.com>
2023-11-13 17:08:58 +0000

    integration de la page de connexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './authentification/auth/auth.component';
+import { AcceuilAdminComponent } from './admin/acceuil-admin/acceuil-admin.component';
+import { AcceuilFormateurComponent } from './formateur/acceuil-formateur/acceuil-formateur.component';
+import { AcceuilApprenantComponent } from './apprenant/acceuil-apprenant/acceuil-apprenant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'connexion', component:AuthComponent},
+  {path: 'acceuil-admin', component:AcceuilAdminComponent},
+  {path: 'acceuil-formateur', component:AcceuilFormateurComponent},
+  {path: 'acceuil-apprenant', component:AcceuilApprenantComponent},
+  {path: '', redirectTo: 'connexion', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './authentification/auth/auth.component';
+import { FormsModule } from '@angular/forms';
+import { AcceuilApprenantComponent } from './apprenant/acceuil-apprenant/acceuil-apprenant.component';
+import { AcceuilFormateurComponent } from './formateur/acceuil-formateur/acceuil-formateur.component';
+import { AcceuilAdminComponent } from './admin/acceuil-admin/acceuil-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    AcceuilApprenantComponent,
+    AcceuilFormateurComponent,
+    AcceuilAdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
